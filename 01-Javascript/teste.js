a3dae7e648a8a5202a7d269e0ea22546683b3f90
Enzo-Aita <Enzoaitanaime99@gmail.com>
2026-04-16 22:12:05 -0300

    Primeiro projeto

---
+++ 01-Javascript/teste.js
@@ -0,0 +1,35 @@
+//alert("Olá Mundo")
+//alert("SEGUNDA MENSAGE")
+
+// let nome 
+// nome = "Enzo Aita"
+// let idade = 19
+//alert("Nome: "+ nome + "\nIdade:" + idade)
+
+// let nome = prompt("Digite seu nome:")
+// let idade = prompt("Digite sua idade:")
+// alert("nome: " + nome + "\nidade: " + idade)
+
+
+// let nota1 = prompt("Digite a nota 1: ")
+// let nota2 = prompt("digite a nota 2:")
+// alert("Nota 1: " + nota1 + "\nNota 2: " + nota2)
+// nota1 = Number(nota1)
+// nota1 = nota1 + 1
+// nota2 = Number(nota2)
+// alert("Nova Nota: " + nota1)
+// let = soma
+// soma = nota1 + nota2
+// alert("Soma das Notas é: " + soma)
+
+let nota1 = prompt("Digite a nota 1: ")
+let nota2 = prompt("Digite a nota 2: ")
+
+// Convertendo ambas para número
+nota1 = Number(nota1)
+nota2 = Number(nota2)
+
+// Criando uma variável para armazenar a soma
+let soma = nota1 + nota2
+
+alert("A soma das notas é: " + soma)
