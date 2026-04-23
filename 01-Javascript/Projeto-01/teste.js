a66cf434394cca9506371264e8c0611c6466c5eb
Enzo-Aita <Enzoaitanaime99@gmail.com>
2026-04-23 20:31:28 -0300

    Projeto 01

--- 01-Javascript/Projeto-01/teste.js
+++ 01-Javascript/Projeto-01/teste.js
@@ -10,19 +10,10 @@
 // let idade = prompt("Digite sua idade:")
 // alert("nome: " + nome + "\nidade: " + idade)
 
-let nota1 = prompt("Digite a nota 1: ")
-let nota2 = prompt("digite a nota 2:")
-alert("Nota 1: " + nota1 + "\nNota 2: " + nota2)
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-nota2 = Number(nota2)
-alert("Nova Nota: " + nota1)
-
-soma = nota1 + nota2
-alert("Soma das Notas é: " + soma)
-
-let nota1 = prompt("Digite a nota 1: ")
-let nota2 = prompt("Digite a nota 2: ")
+// let nota1 = prompt("Digite a nota 1: ")
+// nota1 = Number(nota1)
+// nota1 = nota1 + 1
+// alert("A nota 1 é:"+ nota1)
 
 // Convertendo ambas para número
 // nota1 = Number(nota1)
@@ -30,5 +21,24 @@ let nota2 = prompt("Digite a nota 2: ")
 
 // Criando uma variável para armazenar a soma
 //let soma = nota1 + nota2
-
 //alert("A soma das notas é: " + soma)
+
+
+
+
+//PEBA 01
+//==================================
+// informações 
+let vitorias, empates
+let pontos
+// leitura
+vitorias = prompt("vitórias: ")
+vitorias = Number(vitorias)
+
+empates = Number(prompt("Empates: "))
+// processamentos
+pontos = vitorias*3 + empates
+//saidas
+alert("Total de pontos: " + pontos)
+
+//===================================
