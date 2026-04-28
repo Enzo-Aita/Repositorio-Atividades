6066e0295dbf7e1345befb3759484f84404499c2
Enzo-Aita <Enzoaitanaime99@gmail.com>
2026-04-27 21:18:34 -0300

    talles2.0

--- 01-Javascript/monika.js
+++ 01-Javascript/monika.js
@@ -2,10 +2,9 @@
 let n 
 let chance
 // leitura
-chance = Number(prompt(" chance de para humanos: "))
 n = Number(prompt("Numero de olhadinhas no celuluar: "))
 // processo
 chance = (0.1 / (1 + 500 * n)) * 100
 // saidas
-alert("Chance de aprovação basiada nos olhadinha no celular:  " + chance)
+alert("Chance de aprovação basiada nos olhadinha no celular:  " + chance.toFixed(4))
 
---
+++ 01-Javascript/tallesTransporte2.0.js
@@ -0,0 +1,13 @@
+// infos
+let frete
+let peso
+let distancia
+let volume
+// leitura
+peso = Number(prompt("Peso carga: Kg "))
+distancia = Number(prompt("Distância percorrida: Km"))
+volume = Number(prompt("Volume da carga em m3: "))
+// processo
+frete = 15 + (2 * peso) + (0.05 * distancia ) + (10 * volume)
+// saidas
+console.log("Frete total: R$ " + frete )
