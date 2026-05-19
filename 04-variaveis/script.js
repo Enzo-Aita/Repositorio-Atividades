afe6cdfd42f8ece877c4ce5c824bb17ae7cfd1d7
Enzo-Aita <Enzoaitanaime99@gmail.com>
2026-05-18 21:37:38 -0300

    atualizações

--- 04-variaveis/script.js
+++ 04-variaveis/script.js
@@ -1,18 +1,44 @@
+
+let total = 0
+function registrarVenda() {
+    let valor = Number(document.getElementById("inputVenda").value)
+
+    total = total + valor
+    document.getElementById("totalVendido").innerHTML = "Total de vendas: R$" + total.toFixed(2)
+
+    document.getElementById("inputVenda").value = ""
+    document.getElementById("inputVenda").focus()
+    document.getElementById("listaVendas").innerHTML += "R$" + valor.toFixed(2) + "<br>"
+
+    console.log(valor)
+
+}
 let cont = 0
 function incrementarContagem() {
-    // let cont = 0
     cont = cont + 1
     document.getElementById("p-contagem").innerHTML = cont
 
-} 
-function matarContagem(){
-        // document.getElementById("p-contagem").innerHTML = 
+}
+function decrementarContagem() {
+    cont--
+    document.getElementById("p-contagem").innerHTML = cont
 
 }
+function somar10() {
+    cont = cont + 10
+    document.getElementById("p-contagem").innerHTML = cont
+}
+function somar2() {
+    cont += 2
+    document.getElementById("p-contagem").innerHTML = cont
 
 
+}
+function zerarContagem() {
+    cont = 0
+    document.getElementById("p-contagem").innerHTML = cont
 
-
+}
 let global = "Eu sou um avariável global :/"
 
 function funcao1() {
