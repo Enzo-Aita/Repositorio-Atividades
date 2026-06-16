db805b3c34f2f8846d4b5d82c64335fd8b9f84d5
enzo-aita <enzoaitanaime99@gmail.com>
2026-06-15 22:17:11 -0300

    repetiçao

---
+++ 07-repetição/script.js
@@ -0,0 +1,20 @@
+// let contador = 0 // inicialização
+
+// while(contador < 10){ // condiçõa de repetição
+//     alert("conatador: " + contador)
+//     contador++ // conatagem
+// }
+
+let peso
+
+peso = Number(prompt("Digite o pese: "))
+
+if(peso > 1.1 || peso < 0.9){
+  alert("Fora tolerância do peso")  
+}
+ 
+let contador = 0
+
+while(conatdor < 10 ){
+
+}
