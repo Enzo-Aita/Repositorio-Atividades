7871e43f01c2d3e133017c38d3fbad1d70f5f616
enzo-aita <enzoaitanaime99@gmail.com>
2026-06-30 22:14:22 -0300

    att array 09

--- 09-array/script.js
+++ 09-array/script.js
@@ -1,21 +1,23 @@
 
-
 let produtos = ["Meia", "Pá de corte", "Suco de manga", "leite"]
 let precos = [8, 15, 12, 6]
+let dias = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+let numerosPares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+let frutas = ['banana', 'maçã', 'uva']
 
 
 function verProdutos() {
     // console.log(produtos);
     for (let i = 0; i < produtos.length; i++) {
         let c = i + 1
-        document.getElementById("lista-produtos").innerHTML += "<p>" + c + "º:" + produtos[i] + "</p>"
+        document.getElementById("Resultado").innerHTML += "<p>" + c + "º:" + produtos[i] + "</p>"
     }
 }
 function verProdutoscomPrecos() {
-    document.getElementById("lista-produtos").innerHTML = " "
+    document.getElementById("Resultado").innerHTML = " "
     for (let i = 0; i < produtos.length; i++) {
         let c = i + 1
-        document.getElementById("lista-produtos").innerHTML += "<p>" + c + "º:" + produtos[i] +
+        document.getElementById("Resultado").innerHTML += "<p>" + c + "º:" + produtos[i] +
             " R$ " + precos[i].toFixed(2).replace(".", ",") + "</p>"
     }
 }
@@ -31,12 +33,12 @@ function adicionarnovoProduto() {
     let produto
     let preco
     do {
-        produto =(prompt("digite o nome do produto"))
-        
+        produto = (prompt("digite o nome do produto"))
+
     } while (produto == "");
 
     preco = Number(prompt("Digite o preço do produto"))
-    
+
     produtos.push(produto)
     precos.push(preco)
 
@@ -45,11 +47,70 @@ function adicionarnovoProduto() {
 
 }
 function removerultimoProduto() {
-     produtos.pop() 
-     precos.pop()
-    
+    produtos.pop()
+    precos.pop()
+
 
     verProdutoscomPrecos() //att lista da tela
 
 
 }
+
+function diasdasemana() {
+
+    for (let i = 0; i < dias.length; i++) {
+
+        document.getElementById("Resultado").innerHTML += "<p>" + dias[i] + "</p>"
+
+
+    }
+
+}
+
+function adicionarFeriado() {
+    let feriado
+    feriado = "Feriado"
+
+    dias.push(feriado)
+    diasdasemana()
+}
+function removerUltimoDia() {
+    dias.pop()
+    diasdasemana()
+
+}
+
+function listaNumerosPares() {
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < numerosPares.length; i++) {
+
+        document.getElementById("Resultado").innerHTML += "<p>" + numerosPares[i] + "</p>"
+
+    }
+}
+
+function removerTerceiroNumero() {
+    numerosPares.splice(2, 1, "12")
+
+    listaNumerosPares()
+}
+
+function listadeFrutas() {
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < frutas.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + frutas[i] + "</p>"
+    }
+}
+function adicionarMorango() {
+    let fruta
+    fruta = "morango"
+    frutas.push(fruta)
+
+    listadeFrutas()
+}
+
+
