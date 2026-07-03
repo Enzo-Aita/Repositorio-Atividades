bd4dddb361a0bdb48ae46fb98f6f7e7c07df4113
enzo-aita <enzoaitanaime99@gmail.com>
2026-07-02 21:59:49 -0300

    att array 09

--- 09-array/script.js
+++ 09-array/script.js
@@ -6,6 +6,10 @@ let numerosPares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 let frutas = ['banana', 'maçã', 'uva']
 let amigos = ['Alice', 'Bob', 'Charlie', 'Dave', 'Eva', 'Frank']
 let idades = [25, 30, 28, 22, 26, 24]
+let compras = ['Maçãs', 'Pão', 'Leite']
+let numerosImpares = []
+let numeros = [3, 6, 9]
+let cidades = ['Paris', 'Londres', 'Nova York']
 
 
 function verProdutos() {
@@ -134,6 +138,7 @@ function listaAmigos() {
 
 }
 function adicionarAmigos() {
+
     document.getElementById("Resultado").innerHTML = " "
 
     let amigos2 = ['leo', 'joao', 'kauan']
@@ -183,3 +188,59 @@ function mediaIdades() {
 
 }
 
+function listaDeCompras() {
+
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < compras.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + compras[i] + "</p>"
+    }
+
+}
+function adicionarCompras() {
+
+    document.getElementById("Resultado").innerHTML = " "
+
+    compras.push("ovo", "leite")
+
+    listaDeCompras()
+
+}
+
+function removerTerceiraCompras() {
+
+    compras.splice(2, 1)
+    listaDeCompras()
+
+}
+function testarelemento() {
+    let scanner = compras.includes("pão")
+
+    console.log(scanner)
+
+    listaDeCompras()
+}
+
+function listaDenumerosImpares() {
+    console.log(numerosImpares)
+}
+function adicinarNumerosImpares() {
+
+    for (let i = 1;  numerosImpares.length < 5; i += 2) {
+        numerosImpares.push(i)
+
+    }
+    console.log(numerosImpares)
+}
+
+function multilicador() {
+
+    let multiplicador = numeros.map((x) => x * 2)
+
+    console.log(multiplicador)
+}
+function cidades(){
+    
+}
