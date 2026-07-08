cb5652f72657c06aba7bcd4ace4cf0f6ed45e2f2
enzo-aita <enzoaitanaime99@gmail.com>
2026-07-07 22:09:41 -0300

    Segunda Bateria de Array

--- 10-array/script.js
+++ 10-array/script.js
@@ -1,3 +1,4 @@
+// Primeira Bateria de Array
 let personagem = ["Lúcio Fernando", "Mônica", "Capitão Ganso"]
 let personagem2 = ["Lúcio Fernando", "Mônica", "Capitão Ganso", "Gill Bates"]
 let personagem3 = ["Peba", "Bilu", "Waldisney"]
@@ -238,7 +239,7 @@ function exercicio25() {
     let inclui = personagem8.includes("Kowalski")
     console.log(inclui);
 
-     let indece = personagem11.indexOf("Padre Ernan Buco")
+    let indece = personagem11.indexOf("Padre Ernan Buco")
     console.log(indece);
 
     personagem11.splice(2, 1)
@@ -247,4 +248,102 @@ function exercicio25() {
 
     listaPersonagens11()
 }
+// Segunda Bateria de Array
 
+let personagem12 = ["Dona Bete", "Junin", "Gael", "Mônika", "Gill Bates"];
+let empresas = ["Macrohard", "Hotel Téo", "Pizza Ruth", "HD Cimentos", "Navegano", "Roller Store", "Be a Bar", "Indra", "Acarajéssica", "Reciclaudio"];
+let personagem14 = ["Kowalski", "Heitor Tuga", "Bilu", "Rita Aline", "Caio Pontes", "PeBa"];
+let empresas2 = ["Telles Transportes", "Rede Eco", "Pousada Ada", "Resort Orth", "Bergamota", "Amazonia", "Coxão do Santinho", "Casa de Pastel", "Rango Star", "Alibebe", "Ed Som", "Gê Estantes"];
+let pessoas = ["Márcia", "Mano Juca", "Lúcio Fernando", "Roque", "Yasmina", "Waldisney", "Lady Murphy"];
+let empresas3 = ["Harry Plotter","Albergue Berg","Camping Pim","Ron Bernardo Pet Shop","Visionery","Armando Guerra","Plumas de Avalon", "Romero Brique", "Meia Meia Meia", "Taberna Periódica", "Tião Megastore", "Milhion Pamonharia", "Templo de Salmão"];
+
+function listaPersonagens12() {
+
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < personagem12.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + personagem12[i] + "</p>"
+    }
+}
+function exercicio26() {
+    personagem12.push("Capitão Ganso")
+    listaPersonagens12()
+}
+function listaEmpresa() {
+
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < empresas.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + empresas[i] + "</p>"
+    }
+}
+function exercicio27() {
+    empresa.shift()
+    let indece = empresa.indexOf("HD Cimentos")
+    console.log(indece);
+    listaEmpresas()
+
+}
+function listaPersonagens14() {
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < personagem14.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + personagem14[i] + "</p>"
+    }
+}
+function exercicio28() {
+    personagem14.unshift("Frei Dimão")
+    personagem14.pop()
+    listaPersonagens14()
+}
+function listaEmpresas2() {
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < empresas2.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + empresas2[i] + "</p>"
+    }
+}
+function exercicio29() {
+    empresa2.splice(5, 1)
+    let indece = empresa2.indexOf("Amazonia")
+    console.log(indece)
+    listaEmpresas2()
+}
+function listaPessoas() {
+    document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < pessoas.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + pessoas[i] + "</p>"
+    }
+}
+function exercicio30() {
+    pessoas.reverse()
+    let indece = pessoas.indexOf("Roque")
+    console.log(indece)
+    listaPessoas()
+}
+function listaEmpresas3() {
+  document.getElementById("Resultado").innerHTML = " "
+
+    for (let i = 0; i < empresas3.length; i++) {
+
+
+        document.getElementById("Resultado").innerHTML += "<p>" + empresas3[i] + "</p>"
+    }
+}
+function exercicio30(){
+    empresas3.push("Banda Jota Qwert")
+    empresas3.unshift("Rock in Reels")
+    empresas3.reverse()
+    listaEmpresas3()
+}
